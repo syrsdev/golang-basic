03c696ed10b71bc67b20683c3b977e16bc082648
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-09 23:26:40 +0700

    datatype boolean

---
+++ boolean/bool.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	isSunny := true
+	var hasPermission bool = false
+
+	fmt.Printf("Is it sunny today? %v\n", isSunny)
+	fmt.Printf("Do I have permission? %v\n", hasPermission)
+}
