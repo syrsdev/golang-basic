83702b94a1f97ee3ecc79620eb7142b2e9a23784
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-10 00:43:52 +0700

    datatype string

---
+++ string/string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// interpreted string
+	var name = "syrsdev"
+	pesan := "selamat datang king"
+
+	// raw string
+	var paragraf = `ini adalah paragraf
+	kiw jeger`
+
+	fmt.Println(name)
+	fmt.Println(pesan)
+	fmt.Println(paragraf)
+}
