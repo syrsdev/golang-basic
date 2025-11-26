183d1c85ba8f173c656da56423c10696af8e1005
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-26 13:43:52 +0700

    conversion number to number

---
+++ conversion_number_to_number/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b float64 = float64(a) // konversi dari int ke float64
+
+	fmt.Println("nilai a:", a)
+	fmt.Println("nilai b:", b)
+}
